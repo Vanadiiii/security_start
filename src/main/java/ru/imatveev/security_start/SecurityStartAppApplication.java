4a5d4959401bc03d24afc30d312802b8b3d3f22e
Ivan Matveev <vanadiiii42@gmail.com>
2020-12-06 18:35:51 +0400

    Initial commit

---
+++ src/main/java/ru/imatveev/security_start/SecurityStartAppApplication.java
@@ -0,0 +1,13 @@
+package ru.imatveev.security_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityStartAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/imatveev/security_start/SecurityStartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.imatveev.security_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
